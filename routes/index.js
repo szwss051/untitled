99ae1e894deb4c36701a8702d5b35d3ca9558a7d
szwss051 <q88981909054@163.com>
2021-05-24 10:02:59 +0800

    手机商城 11

--- routes/index.js
+++ routes/index.js
@@ -5,50 +5,104 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-//订单详情
-router.get('/ddxq', function(req, res, next) {
-  res.render('ddxq');
-});
-//地址
-router.get('/dz', function(req, res, next) {
-  res.render('dz');
-});
-//分类
-router.get('/fl', function(req, res, next) {
-  res.render('fl');
-});
-//购买详情
-router.get('/gmxq', function(req, res, next) {
-  res.render('gmxq');
-});
-//个人资料
-router.get('/grzl', function(req, res, next) {
-  res.render('grzl');
-});
-//购物车
-router.get('/gwc', function(req, res, next) {
-  res.render('gwc');
-});
-//会员中心
-router.get('/hy', function(req, res, next) {
-  res.render('hy');
-});
-//收藏
-router.get('/sc', function(req, res, next) {
-  res.render('sc');
-});
+
 //搜索
 router.get('/ss', function(req, res, next) {
   res.render('ss');
 });
+
+//分类
+router.get('/fl', function(req, res, next) {
+  res.render('fl');
+});
+
 //搜索商品页面1
 router.get('/ss1', function(req, res, next) {
   res.render('ss1');
 });
+
 //搜索商品页面2
 router.get('/ss2', function(req, res, next) {
   res.render('ss2');
 });
+
+
+//购买详情
+router.get('/spxq', function(req, res, next) {
+  res.render('spxq');
+});
+
+//购物车
+router.get('/gwc', function(req, res, next) {
+  res.render('gwc');
+});
+//购物车编辑
+router.get('/gwcbj', function(req, res, next) {
+  res.render('gwcbj');
+});
+
+//收藏
+router.get('/sc', function(req, res, next) {
+  res.render('sc');
+});
+
+
+
+
+
+
+//待付款
+router.get('/dfk', function(req, res, next) {
+  res.render('dfk');
+});
+
+//待发货
+router.get('/dfh', function(req, res, next) {
+  res.render('dfh');
+});
+
+//待收货
+router.get('/dsh', function(req, res, next) {
+  res.render('dsh');
+});
+
+//待评价
+router.get('/dpj', function(req, res, next) {
+  res.render('dpj');
+});
+
+//商品订单
+router.get('/spdd', function(req, res, next) {
+  res.render('spdd');
+});
+
+
+
+
+
+
+
+//地址
+router.get('/dz', function(req, res, next) {
+  res.render('dz');
+});
+
+
+
+//个人资料
+router.get('/grzl', function(req, res, next) {
+  res.render('grzl');
+});
+
+
+
+//会员中心
+router.get('/hy', function(req, res, next) {
+  res.render('hy');
+});
+
+
+
 //消息
 router.get('/xx', function(req, res, next) {
   res.render('xx');
